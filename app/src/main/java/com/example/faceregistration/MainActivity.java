7d13e63bbbb8e9e6e6ab6aee1b08e3ceb05f0373
HyeminNam <intern18@ldcc.kr>
2020-02-11 15:04:50 +0900

    Modified MainActivity submit button

--- app/src/main/java/com/example/faceregistration/MainActivity.java
+++ app/src/main/java/com/example/faceregistration/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -71,6 +74,67 @@ public class MainActivity extends AppCompatActivity {
         final RadioButton female = findViewById(R.id.female);
         final CheckBox agree = findViewById(R.id.agree);
 
+        EditText.OnKeyListener keyListener = new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                String name = edname.getText().toString();
+                String birth = edbirth.getText().toString();
+                String email = edemail.getText().toString();
+
+                // 개인정보 입력 확인
+                if(name.length()==0 || birth.length()==0 || email.length()==0 || (!male.isChecked() && !female.isChecked()) || !agree.isChecked() ){
+                    next.setEnabled(false);
+                    next.setBackgroundResource(R.color.dont);
+                    return false;
+                } else{
+                    next.setEnabled(true);
+                    next.setBackgroundResource(R.color.button);
+                    return true;
+                }
+            }
+        };
+
+        edname.setOnKeyListener(keyListener);
+        edbirth.setOnKeyListener(keyListener);
+        edemail.setOnKeyListener(keyListener);
+
+        RadioGroup radioGroup = findViewById(R.id.gender_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                String name = edname.getText().toString();
+                String birth = edbirth.getText().toString();
+                String email = edemail.getText().toString();
+
+                // 개인정보 입력 확인
+                if(name.length()==0 || birth.length()==0 || email.length()==0 || (!male.isChecked() && !female.isChecked()) || !agree.isChecked() ){
+                    next.setEnabled(false);
+                    next.setBackgroundResource(R.color.dont);
+                } else{
+                    next.setEnabled(true);
+                    next.setBackgroundResource(R.color.button);
+                }
+            }
+        });
+
+        agree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edname.getText().toString();
+                String birth = edbirth.getText().toString();
+                String email = edemail.getText().toString();
+
+                // 개인정보 입력 확인
+                if(name.length()==0 || birth.length()==0 || email.length()==0 || (!male.isChecked() && !female.isChecked()) || !agree.isChecked() ){
+                    next.setEnabled(false);
+                    next.setBackgroundResource(R.color.dont);
+                } else{
+                    next.setEnabled(true);
+                    next.setBackgroundResource(R.color.button);
+                }
+            }
+        });
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,46 +143,23 @@ public class MainActivity extends AppCompatActivity {
                 String email = edemail.getText().toString();
                 String gender = "";
                 String agreement = "";
-                boolean flag = false;
 
-                // 개인정보 입력 확인
-                if(name.length()==0){
-                    Toast.makeText(MainActivity.this, "이름을 입력하세요.", Toast.LENGTH_SHORT).show();
-                } else if(birth.length()==0){
-                    Toast.makeText(MainActivity.this, "생년월일을 입력하세요.", Toast.LENGTH_SHORT).show();
-                } else if(email.length()==0){
-                    Toast.makeText(MainActivity.this, "이메일을 입력하세요.", Toast.LENGTH_SHORT).show();
-                } else if(!male.isChecked() && !female.isChecked()){
-                    Toast.makeText(MainActivity.this, "성별을 선택해주세요.", Toast.LENGTH_SHORT).show();
-                } else if(!agree.isChecked()){
-                    Toast.makeText(MainActivity.this, "동의함을 체크해주세요.", Toast.LENGTH_SHORT).show();
-                } else{
-                    flag = true;
-                }
-
-                // 남자, 여자 체크 값 가져오기
                 if(male.isChecked()){
                     gender = "M";
                 } else if(female.isChecked()){
                     gender = "F";
                 }
 
-                // 동의함 체크 여부
                 if(agree.isChecked()){
                     agreement = "Y";
                 }
 
-                if(flag){
-                    next.setEnabled(true);
-                    next.setBackgroundResource(R.color.button);
-                    Privacy privacy = new Privacy(name, birth, email, gender, agreement);
-                    Intent intentSend = new Intent(getApplicationContext(), Simulation.class);
-                    intentSend.putExtra("class", privacy);
-                    startActivity(intentSend);
-                    finish();
-                }
-
-
+                Privacy privacy = new Privacy(name, birth, email, gender, agreement);
+                Intent intentSend = new Intent(getApplicationContext(), Simulation.class);
+                Toast.makeText(MainActivity.this, name+birth+email+gender+agreement, Toast.LENGTH_SHORT).show();
+                intentSend.putExtra("class", privacy);
+                startActivity(intentSend);
+                finish();
             }
         });
     }
